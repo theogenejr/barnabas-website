ac2dfefe5dafdd4d3bf1a47eb4b10f2cb0dcd15e
theogenejr <theogenejr@gmail.com>
2024-02-16 12:08:39 +0300

    Hero Bg

--- src/components/HeroBg.js
+++ src/components/HeroBg.js
@@ -15,7 +15,6 @@ const HeroBg = () => {
       <StaticImage
         src="../images/bg3.jpeg"
         alt="hero image"
-        loading="eager"
         className=" h-full w-full object-cover"
       />
     </motion.div>
