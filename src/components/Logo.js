a03ad90a931094bf75ae0839e1643fe5f7832a43
theogenejr <theogenejr@gmail.com>
2024-02-16 13:23:36 +0300

    logos wrapeed

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,22 +5,26 @@ import React from "react"
 const Logo = () => {
   return (
     <div className="flex items-center justify-center md:mt-2">
-      <Link to="/">
-        {/* Logo for smaller screens */}
-        <StaticImage
-          src="../images/sm-logo-trans.png"
-          alt="Mobile Logo"
-          className="w-12 h-auto md:hidden" // Hide on medium and larger screens
-        />
-      </Link>
-      <Link to="/">
-        {/* Logo for larger screens */}
-        <StaticImage
-          src="../images/lg-logo-trans.png"
-          alt="Desktop Logo"
-          className="hidden md:block w-32 h-auto" // Show on medium and larger screens
-        />
-      </Link>
+      <div className="w-12 h-auto md:hidden">
+        <Link to="/">
+          {/* Logo for smaller screens */}
+          <StaticImage
+            src="../images/sm-logo-trans.png"
+            alt="Mobile Logo"
+            className="" // Hide on medium and larger screens
+          />
+        </Link>
+      </div>
+      <div className="hidden md:block w-32 h-auto">
+        <Link to="/">
+          {/* Logo for larger screens */}
+          <StaticImage
+            src="../images/lg-logo-trans.png"
+            alt="Desktop Logo"
+            className="" // Show on medium and larger screens
+          />
+        </Link>
+      </div>
     </div>
   )
 }
