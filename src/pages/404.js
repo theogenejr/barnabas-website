f73fb6fb7f7e9997d6bf047f1e6f985be40589ed
theogenejr <theogenejr@gmail.com>
2024-02-18 19:51:53 +0300

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,23 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="flex flex-col gap-3 h-screen w-screen justify-center items-center">
+      <span className="text-6xl font-black">404</span>
+      <span className="text-4xl font-bold text-goodBlue">Page not found</span>
+      <div className="flex flex-col items-center font-semibold text-lg">
+        <span className="">Looks like somebody pressed the wrong button</span>
+        <span className="">Sorry for the incovenience</span>
+      </div>
+      <Link to="/">
+        <button className="bg-goodBlue uppercase px-8 py-3 mt-8 rounded-full font-bold text-white">
+          go back home
+        </button>
+      </Link>
+    </div>
   </Layout>
 )
 
