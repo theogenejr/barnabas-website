0c5895dca9ecea76a0974930cace8ac84fb3617e
TheogeneJR <153107357+theogenejr@users.noreply.github.com>
2024-02-21 19:53:13 +0300

    music page 3rd commit

--- src/pages/music.js
+++ src/pages/music.js
@@ -4,88 +4,190 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { StaticImage } from "gatsby-plugin-image"
+import Subscription from "../components/Subscription"
 
-const Music = () => (
-  <Layout>
-    <div className="relative">
-      <div className="h-[calc(100vh-80px)] rounded-3xl overflow-hidden relative my-2 mx-4">
-        <div className="w-full h-full bg-black/50 rounded-3xl">
-          <StaticImage
-            src="../images/music1.png"
-            alt="hero image"
-            className=" h-full w-full object-cover rounded-3xl grayscale"
-          />
-        </div>
-        <div className="absolute w-full h-full bg-black/50 top-0 left-0 flex items-start pt-48 sm:pt-32 sm:pl-24 pl-6">
-          <div className="flex relative">
-            <span className="uppercase text-white text-6xl sm:text-8xl lg:text-9xl font-black z-20">
-              Music
-            </span>
-            <div className="bg-blue-800/50 sm:w-52 w-32 h-32  sm:h-52 md:w-72 md:h-72 absolute right-1/2 left-1/2 -top-10 sm:-right-36 md:-right-56 lg:-right-52 rounded-full z-10 sm:-top-14 md:-top-24 lg:-top-20"></div>
+const Music = () => {
+  const tracks = [
+    {
+      trackNumber: "01",
+      title: "sabella",
+      duration: "05:30",
+      album: "sambara",
+    },
+    {
+      trackNumber: "02",
+      title: "sawa",
+      duration: "03:22",
+      album: "sambara",
+    },
+    {
+      trackNumber: "03",
+      title: "sabi",
+      duration: "03:33",
+      album: "sambara",
+    },
+    {
+      trackNumber: "04",
+      title: "stakalu",
+      duration: "04:00",
+      album: "sambara",
+    },
+    {
+      trackNumber: "05",
+      title: "suluta wa fata",
+      duration: "05:26",
+      album: "sambara",
+    },
+    {
+      trackNumber: "06",
+      title: "sejerina",
+      duration: "03:57",
+      album: "sambara",
+    },
+    {
+      trackNumber: "07",
+      title: "sambara",
+      duration: "03:55",
+      album: "sambara",
+    },
+    {
+      trackNumber: "08",
+      title: "saba al ker",
+      duration: "04:00",
+      album: "sambara",
+    },
+    {
+      trackNumber: "08",
+      title: "setetu",
+      duration: "04:31",
+      album: "sambara",
+    },
+  ]
+  return (
+    <Layout>
+      <div className="relative mx-4">
+        <div className="h-[calc(100vh-80px)] rounded-3xl overflow-hidden relative my-2 ">
+          <div className="w-full h-full bg-black/50 rounded-3xl">
+            <StaticImage
+              src="../images/music1.png"
+              alt="hero image"
+              className=" h-full w-full object-cover rounded-3xl grayscale"
+            />
           </div>
-        </div>
-      </div>
-      <div className="absolute top-[72vh]  w-full flex flex-col items-center">
-        <span className="uppercase text-white text-sm mb-2">
-          scroll to discover
-        </span>
-        <div className="w-[2px] h-28 bg-blue-700 items"></div>
-      </div>
-      <div className="w-full flex flex-col px-4 pt-16 sm:pt-24 items-center">
-        <p className="md:w-[65%] uppercase text-xl font-light text-slate-700">
-          Welcome to my musical realm. I approach music with a passion to play
-          and to rhythmically express myself. My music is a reflection of my
-          life experiences, as I draw inspiration from the events happening in
-          my world. Through my lyrics, I am able to channel the emotions that
-          arise from the circumstances and injustices that I or my community
-          have faced. My repertoire includes songs about peace, the impact of
-          war, anti-war music, displacement, and the hardships that I have
-          endured while living on the streets, in ghettos, and refugee camps. I
-          also sing about the trials and tribulations of living in extreme
-          poverty, and the hopes that I have for a better future. Moreover, my
-          music delves into the nuances of community life, love, and raising
-          awareness for the struggles of the oppressed.
-        </p>
-        <p className="md:w-[65%] uppercase text-xl font-light text-slate-700 mt-8">
-          As a community activist, I use my art to amplify the voices of those
-          who have been silenced, and to bring their issues to the forefront. My
-          music is a call to action, a rallying cry for change. Come, take a
-          listen, and let us explore the highs and lows of life, and the rhythm
-          that unites us all. - <b>Barnabas Samuel</b>
-        </p>
-      </div>
-      <div className="w-full flex flex-col items-center my-4">
-        <div className="w-[2px] h-40 bg-blue-700 items"></div>
-      </div>
-
-      <div className="w-full items-center my-4 flex flex-col">
-        <div className="uppercase text-3xl font-bold flex gap-2">
-          <span>featured</span>
-          <span className="text-goodBlue">albums</span>
-        </div>
-        <div className="flex w-full justify-center mt-6">
-          <div className="md:w-1/2 lg:w-1/3 w-2/3">
-            <div className="">
-              <div className="">
-                <StaticImage
-                  src="../images/musiccover01.png"
-                  alt="sambara music cover"
-                />
-              </div>
-              <div className="flex flex-col mt-4">
-                <span className="text-2xl uppercase font-bold  ">Sambara</span>
-                <span className="font-medium">2nd September, 2021</span>
-              </div>
-              <div className="flex flex-col mt-3">
-                <span className="capitalize text-goodBlue">listen on</span>
-              </div>
+          <div className="absolute w-full h-full bg-black/50 top-0 left-0 flex items-start pt-48 sm:pt-32 sm:pl-24 pl-6">
+            <div className="flex relative">
+              <span className="uppercase text-white text-6xl sm:text-8xl lg:text-9xl font-black z-20">
+                Music
+              </span>
+              <div className="bg-blue-800/50 sm:w-52 w-32 h-32  sm:h-52 md:w-72 md:h-72 absolute right-1/2 left-1/2 -top-10 sm:-right-36 md:-right-56 lg:-right-52 rounded-full z-10 sm:-top-14 md:-top-24 lg:-top-20"></div>
             </div>
           </div>
         </div>
+        <div className="absolute top-[72vh]  w-full flex flex-col items-center">
+          <span className="uppercase text-white text-sm mb-2">
+            scroll to discover
+          </span>
+          <div className="w-[2px] h-28 bg-blue-700 items"></div>
+        </div>
+        <div className="w-full flex flex-col px-4 pt-16 sm:pt-24 items-center">
+          <p className="md:w-[65%] uppercase text-xl font-light text-slate-700">
+            Welcome to my musical realm. I approach music with a passion to play
+            and to rhythmically express myself. My music is a reflection of my
+            life experiences, as I draw inspiration from the events happening in
+            my world. Through my lyrics, I am able to channel the emotions that
+            arise from the circumstances and injustices that I or my community
+            have faced. My repertoire includes songs about peace, the impact of
+            war, anti-war music, displacement, and the hardships that I have
+            endured while living on the streets, in ghettos, and refugee camps.
+            I also sing about the trials and tribulations of living in extreme
+            poverty, and the hopes that I have for a better future. Moreover, my
+            music delves into the nuances of community life, love, and raising
+            awareness for the struggles of the oppressed.
+          </p>
+          <p className="md:w-[65%] uppercase text-xl font-light text-slate-700 mt-8">
+            As a community activist, I use my art to amplify the voices of those
+            who have been silenced, and to bring their issues to the forefront.
+            My music is a call to action, a rallying cry for change. Come, take
+            a listen, and let us explore the highs and lows of life, and the
+            rhythm that unites us all. - <b>Barnabas Samuel</b>
+          </p>
+        </div>
+        <div className="w-full flex flex-col items-center my-4">
+          <div className="w-[2px] h-40 bg-blue-700 items"></div>
+        </div>
+
+        <div className="w-full items-center my-4 flex flex-col">
+          <div className="uppercase text-3xl font-bold flex gap-2">
+            <span>featured</span>
+            <span className="text-goodBlue">albums</span>
+          </div>
+          <div className="flex w-full justify-center mt-6">
+            <div className="md:w-1/2 lg:w-1/3 w-2/3">
+              <div className="">
+                <div className="">
+                  <StaticImage
+                    src="../images/musiccover01.png"
+                    alt="sambara music cover"
+                  />
+                </div>
+                <div className="flex flex-col mt-4">
+                  <span className="text-2xl uppercase font-bold  ">
+                    Sambara
+                  </span>
+                  <span className="font-medium">2nd September, 2021</span>
+                </div>
+                <div className="flex flex-col mt-3">
+                  <span className="capitalize text-goodBlue font-semibold">
+                    listen on
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className=""></div>
+          </div>
+        </div>
+        <div className="w-full flex flex-col items-center my-4">
+          <div className="w-[2px] h-40 bg-blue-700 items"></div>
+        </div>
+        <div className="w-full justify-center uppercase text-3xl font-bold flex gap-2">
+          <span>latest</span>
+          <span className="text-goodBlue">tracks</span>
+        </div>
+        <div className="w-full bg-goodBlue/5 my-4 rounded-3xl py-8 px-4">
+          <div className="border-t-[1px] flex flex-col border-goodBlue/50">
+            {tracks.map(track => (
+              <div
+                key={track.trackNumber}
+                className="flex justify-between border-b-[1px] border-goodBlue/50 py-4 cursor-pointer hover:bg-goodBlue/10 sm:px-4 px-2"
+              >
+                <div className="w-1/2">
+                  <span className="font-bold mr-4 sm:mr-16">
+                    {track.trackNumber}
+                  </span>
+                  <span className="font-bold text-xl text-slate-700 mr-2 whitespace-nowrap text-ellipses overflow-hidden">
+                    {track.title}
+                  </span>
+                  <span className="text-gray-700 text-sm">
+                    {track.duration}
+                  </span>
+                </div>
+                <div className="w-1/2 hidden  md:flex justify-end whitespace-nowrap">
+                  <span className="uppercase">
+                    <span className="font-semibold mr-[2px]">
+                      {track.album}
+                    </span>{" "}
+                    <span className="text-sm">album</span>
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <Subscription />
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export const Head = () => <Seo title="Music" />
 
