9e71a1030f06a536dcf4933b91c29bd6f220612f
TheogeneJR <153107357+theogenejr@users.noreply.github.com>
2024-02-21 17:24:30 +0300

    music page 02 commit

--- src/pages/music.js
+++ src/pages/music.js
@@ -16,7 +16,7 @@ const Music = () => (
             className=" h-full w-full object-cover rounded-3xl grayscale"
           />
         </div>
-        <div className="absolute w-full h-full bg-black/50 top-0 left-0 flex items-start pt-24 sm:pl-24 pl-6">
+        <div className="absolute w-full h-full bg-black/50 top-0 left-0 flex items-start pt-48 sm:pt-32 sm:pl-24 pl-6">
           <div className="flex relative">
             <span className="uppercase text-white text-6xl sm:text-8xl lg:text-9xl font-black z-20">
               Music
@@ -31,7 +31,7 @@ const Music = () => (
         </span>
         <div className="w-[2px] h-28 bg-blue-700 items"></div>
       </div>
-      <div className="w-full flex flex-col px-4 py-24 items-center">
+      <div className="w-full flex flex-col px-4 pt-16 sm:pt-24 items-center">
         <p className="md:w-[65%] uppercase text-xl font-light text-slate-700">
           Welcome to my musical realm. I approach music with a passion to play
           and to rhythmically express myself. My music is a reflection of my
@@ -53,6 +53,35 @@ const Music = () => (
           listen, and let us explore the highs and lows of life, and the rhythm
           that unites us all. - <b>Barnabas Samuel</b>
         </p>
+      </div>
+      <div className="w-full flex flex-col items-center my-4">
+        <div className="w-[2px] h-40 bg-blue-700 items"></div>
+      </div>
+
+      <div className="w-full items-center my-4 flex flex-col">
+        <div className="uppercase text-3xl font-bold flex gap-2">
+          <span>featured</span>
+          <span className="text-goodBlue">albums</span>
+        </div>
+        <div className="flex w-full justify-center mt-6">
+          <div className="md:w-1/2 lg:w-1/3 w-2/3">
+            <div className="">
+              <div className="">
+                <StaticImage
+                  src="../images/musiccover01.png"
+                  alt="sambara music cover"
+                />
+              </div>
+              <div className="flex flex-col mt-4">
+                <span className="text-2xl uppercase font-bold  ">Sambara</span>
+                <span className="font-medium">2nd September, 2021</span>
+              </div>
+              <div className="flex flex-col mt-3">
+                <span className="capitalize text-goodBlue">listen on</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </Layout>
